5d9f4c164b80f607471f84b991f1b163224ca9ea
lesleyjwu <98455428+lesleyjwu@users.noreply.github.com>
2024-04-07 04:56:38 -0400

    Update demo.py with image path

--- demo.py
+++ demo.py
@@ -9,7 +9,7 @@ import cv2
 from rmn import RMN
 
 
-image = cv2.imread("some-image-path.png")
+image = cv2.imread("demo.png")
 m = RMN()
 results = m.detect_emotion_for_single_frame(image)
 print(results)
