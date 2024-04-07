4d3dbd72395fc754e93b0fcb3175f836b3e94b37
lesleyjwu <98455428+lesleyjwu@users.noreply.github.com>
2024-04-07 04:44:46 -0400

    add demo files

---
+++ demo.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+import cv2
+from rmn import RMN
+
+
+image = cv2.imread("some-image-path.png")
+m = RMN()
+results = m.detect_emotion_for_single_frame(image)
+print(results)
+image = m.draw(image, results)
+cv2.imwrite("output.png", image)
+
